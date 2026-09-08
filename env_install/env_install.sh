2ce9d07eb915b07d7ddc1c91334307b028a602c2
Yihan Pang <yihanp2@illinois.edu>
2026-09-07 21:55:22 -0500

    Remove unused torchaudio dependency

--- env_install/env_install.sh
+++ env_install/env_install.sh
@@ -7,7 +7,7 @@ pip install trimesh
 pip install rtree 
 pip install pyrender
 
-conda install -y pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install -y pytorch==2.4.0 torchvision==0.19.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install stannum
 pip install termcolor
 pip install fvcore
